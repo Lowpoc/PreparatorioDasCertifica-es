485c18f1e8e07f8d8263ead6f8f21e7c36b892d3
marcus.santana <marcus.santana@paggcerto.com.br>
2019-11-25 17:13:14 -0300

    Use keywords async and await

---
+++ ExamRef70-483ProgramminginC#/Chapter1/ManageProgamFlow/ImplementMultTreadingAndAsynchronousProcessing/UseAsyncAndAwait/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace UseAsyncAndAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DownloadContent().Result);
+        }
+
+
+        static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string result = await client.GetStringAsync("https://www.microsoft.com");
+                return result;
+            }   
+        }
+    }
+}
