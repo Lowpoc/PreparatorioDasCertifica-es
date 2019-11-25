fa3bd6e2446e05a8244a8fe32158849220e79648
marcus.santana <marcus.santana@paggcerto.com.br>
2019-11-25 16:37:18 -0300

    Add study UseParallel class

---
+++ ExamRef70-483ProgramminginC#/Chapter1/ManageProgamFlow/ImplementMultTreadingAndAsynchronousProcessing/UseParallelClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace UseParallelClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 10, i =>
+            {
+                Thread.Sleep(1000);
+            });
+
+            var numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i =>
+            {
+                Thread.Sleep(1000);
+            });
+
+            ParallelLoopResult result = Parallel.For(0, 600, (int i, ParallelLoopState loopstate) =>
+            {
+                if (i == 500)
+                {
+                    Console.WriteLine("Break loop parellel.");
+                    loopstate.Break();
+                }
+
+                return;
+            });
+        }
+    }
+}
